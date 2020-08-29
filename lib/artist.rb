75162f1749a61863035ca2a2999ea67c1de88e93
Kenneth Colon <kennethmanuel250@gmail.com>
2020-08-29 03:06:55 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,17 @@
+class Song 
+  attr_accessor :title, :artist 
+  
+  def initialize(title)
+    @title = title
+  end 
+end 
+
+class Artist 
+  attr_accessor :name
+  
+  def initialize(name)
+    @name = name
+  end 
+end 
+
+beyonce = Artist.new("Beyonce")
